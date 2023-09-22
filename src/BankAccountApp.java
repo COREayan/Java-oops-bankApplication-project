a73e71ad09233028c8ffc08dcbfba8acd20a99ee
COREayan <63402298+COREayan@users.noreply.github.com>
2023-09-22 23:19:37 +0530

    First Commit

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -2,16 +2,13 @@ public class BankAccountApp {
     public static void main(String[] args) {
 
         Checking chk = new Checking("Tom Wilson", "321456879", 1500);
-
         Savings sav = new Savings("Rich Lowe", "456657897", 2500);
+
+        sav.compound();
 
         sav.showInfo();
         System.out.println("*************************************************************");
         chk.showInfo();
-
-        sav.deposit(5000);
-        sav.withdraw(200);
-        sav.transfer("brokerage", 3000);
 
         // Read a CSV File then create new accounts based on that data
     }
--- src/IBaseRate.java
+++ src/IBaseRate.java
@@ -5,4 +5,6 @@ public interface IBaseRate {
         return 2.5;
     }
 
+
+
 }
